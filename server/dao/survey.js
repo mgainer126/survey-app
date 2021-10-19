bc556a1034345e8144082eeae73340a2157b9452
Matt Gainer <matt.gainer126@hotmail.com>
2021-10-19 13:09:55 -0400

    removed server side error message, where return was not necessary

--- server/dao/survey.js
+++ server/dao/survey.js
@@ -2,16 +2,14 @@ const db = require("../db/db");
 
 class SurveyDAO {
   async createSurvey(employee, comment, knowledge, prepared, priority, value) {
-    const [id] = await db("survey")
-      .insert({
-        employee: employee,
-        comment: comment,
-        knowledge: knowledge,
-        prepared: prepared,
-        priority: priority,
-        value: value,
-      })
-      .returning("id");
+    const [id] = await db("survey").insert({
+      employee: employee,
+      comment: comment,
+      knowledge: knowledge,
+      prepared: prepared,
+      priority: priority,
+      value: value,
+    });
     return id;
   }
 }
