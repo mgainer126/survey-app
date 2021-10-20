4fc16c943af4e320e679ddc2fc4431eccbd88217
Matt Gainer <matt.gainer126@hotmail.com>
2021-10-20 17:26:42 -0400

    updates to the result page to show results of the survey

--- client/src/components/Stats/Stats.js
+++ client/src/components/Stats/Stats.js
@@ -1,36 +1,19 @@
-import Table from "react-bootstrap/Table";
+function Stats({ results }) {
+  console.log(results);
 
-function Stats() {
-  return (
-    <>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Employee</th>
-            <th>Inititave ID</th>
-            <th>Prepardness</th>
-            <th>Priority</th>
-            <th>Knowledge</th>
-            <th>Ownership</th>
-            <th>Comment</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Emplyee name</td>
-            <td>Inititave ID</td>
-            <td>Preparnedss Score</td>
-            <td>Priority Score</td>
-            <td>Knowledge Score</td>
-            <td>Ownership Score</td>
-            <td>Comments</td>
-          </tr>
-        </tbody>
-      </Table>
-    </>
-  );
+  return results.map((result) => {
+    return (
+      <tr>
+        <td>{result.employee}</td>
+        <td>{result.initiativeId}</td>
+        <td>{result.prepared}</td>
+        <td>{result.priority}</td>
+        <td>{result.knowledge}</td>
+        <td>{result.ownership}</td>
+        <td>{result.comment}</td>
+      </tr>
+    );
+  });
 }
 
 export default Stats;
--- client/src/pages/StatsPage/StatsPage.js
+++ client/src/pages/StatsPage/StatsPage.js
@@ -1,20 +1,47 @@
 import { React, Component } from "react";
+import Table from "react-bootstrap/Table";
 import Stats from "../../components/Stats/Stats";
 import axios from "axios";
 
 export default class StatsPage extends Component {
-  render() {
+  state = {
+    results: null,
+  };
+
+  componentDidMount() {
     axios
       .get("http://localhost:8080/request/survey")
-      .then(function (response) {
+      .then((response) => {
+        this.setState({ results: response.data });
         console.log(response.data);
       })
       .catch(function (error) {
         console.log(error);
       });
+  }
+
+  render() {
+    console.log(this.state.results);
     return (
       <>
-        <Stats />
+        {this.state.results && this.state.results && (
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Employee</th>
+                <th>Inititave ID</th>
+                <th>Prepardness</th>
+                <th>Priority</th>
+                <th>Knowledge</th>
+                <th>Ownership</th>
+                <th>Comment</th>
+              </tr>
+            </thead>
+            <tbody>
+              <Stats results={this.state.results} />
+            </tbody>
+          </Table>
+        )}
       </>
     );
   }
