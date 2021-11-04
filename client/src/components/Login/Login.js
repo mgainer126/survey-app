6dce2a280f38ebe2312fc960dc59234e6fcd3d7a
Matt Gainer <matt.gainer126@hotmail.com>
2021-11-04 10:53:29 -0400

    added styling for the manager sign in

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -40,22 +40,35 @@ export default function Login() {
   return (
     <div className="login-wrapper">
       <h1>Manager Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <p>Email</p>
-          <input type="email" onChange={(e) => setEmail(e.target.value)} />
+      <form
+        className="p-4 p-md-5 border rounded-3 bg-light login__form"
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="floatingInput">
+          <p>Email Address</p>
+          <input
+            type="email"
+            className="form-control"
+            onChange={(e) => setEmail(e.target.value)}
+          />
           {/* This setUserName entered here will set the userName, that is then created as a token object in line 24   */}
         </label>
-        <label>
+        <label htmlFor="floatingPassword">
           <p>Password</p>
           <input
             type="password"
             onChange={(e) => setPassword(e.target.value)}
+            className="form-control"
             //this setPassword entered here will set the password, that is then created as part of the token object in line 24
           />
         </label>
         <div>
-          <button type="submit">Submit</button>
+          <button
+            type="submit"
+            className="w-100 btn btn-lg btn-primary login__button"
+          >
+            Submit
+          </button>
         </div>
       </form>
     </div>
