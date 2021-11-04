36e15f69ba2592fbdd50570175e98620d6b419a3
Matt Gainer <matt.gainer126@hotmail.com>
2021-11-04 11:32:30 -0400

    styling added to the create PDF button

--- client/src/components/StatsPage/StatsPageColl.js
+++ client/src/components/StatsPage/StatsPageColl.js
@@ -3,7 +3,13 @@ import Table from "react-bootstrap/Table";
 import Stats from "../Stats/Stats";
 import Pdf from "react-to-pdf";
 import React from "react";
+import "./StatsPageColl.scss";
 const ref = React.createRef();
+const options = {
+  orientation: "landscape",
+  unit: "in",
+  format: [15, 13],
+};
 
 function StatsPageColl({ results }) {
   return (
@@ -11,8 +17,12 @@ function StatsPageColl({ results }) {
       <Table striped bordered hover>
         <StatsHeader />
         <Stats results={results} />
-        <Pdf targetRef={ref} filename="employeeResults.pdf">
-          {({ toPdf }) => <button onClick={toPdf}>Capture as PDF</button>}
+        <Pdf targetRef={ref} filename="employeeResults.pdf" options={options}>
+          {({ toPdf }) => (
+            <button className="stats__pdfbtn" onClick={toPdf}>
+              Create PDF
+            </button>
+          )}
         </Pdf>
       </Table>
     </div>
