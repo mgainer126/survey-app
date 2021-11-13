fafb992fc424061d0752e7270e1e689f82502d77
Matt Gainer <matt.gainer126@hotmail.com>
2021-11-13 15:01:31 -0500

    feature where manager login link is dynamic depending if there is a token session present

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,17 +1,27 @@
 import { Link } from "react-router-dom";
 import logo from "../../assets/pclogo.svg";
 import "../Header/Header.scss";
+import useToken from "../../components/App/useToken";
 
 function Header() {
+  const { token } = useToken();
+
   return (
     <>
       <header className="header">
         <Link to="/">
           <img src={logo} alt="logo" className="header__logo"></img>
         </Link>
-        <Link to="/login" className="header__manager">
-          <h5>Manager Sign In</h5>
-        </Link>
+        {!token && (
+          <Link to="/login" className="header__manager">
+            <h5>Manager Log In</h5>
+          </Link>
+        )}
+        {token && (
+          <Link to="/login" className="header__manager">
+            <h5>Manager Log Out</h5>
+          </Link>
+        )}
       </header>
     </>
   );
--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -40,7 +40,6 @@ function Login() {
           });
         }
       })
-
       .catch((error) => {
         console.log(error);
       });
